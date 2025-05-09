3a6bafd15cf0e6bab0c106748315d10b4dabbf0d
GuiCCFRP <monkedrip10@gmail.com>
2025-05-09 20:14:06 +0100

    Update config.py

--- ASSIGNMENT/config.py
+++ ASSIGNMENT/config.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
+#configs
 class Config:
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
