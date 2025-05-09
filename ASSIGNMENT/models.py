0018b73f934debea59b6087b48524ec3846ebf19
GuiCCFRP <monkedrip10@gmail.com>
2025-05-09 20:13:13 +0100

    Update models.py

--- ASSIGNMENT/models.py
+++ ASSIGNMENT/models.py
@@ -1,14 +1,14 @@
-# models.py
+
 from extensions          import db
 from flask_login         import UserMixin
 from datetime            import datetime
-
+# User object and database
 class User(UserMixin, db.Model):
     id       = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email    = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-
+#Database for pdf path history of each user
 class PdfHistory(db.Model):
     id         = db.Column(db.Integer, primary_key=True)
     user_id    = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
