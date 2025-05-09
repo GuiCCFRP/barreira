dc71d530a93c85a293bef35a7bd70d7c43ab5205
GuiCCFRP <monkedrip10@gmail.com>
2025-05-09 20:12:27 +0100

    Update app.py

--- ASSIGNMENT/app.py
+++ ASSIGNMENT/app.py
@@ -1,4 +1,4 @@
-# app.py
+
 from flask        import Flask
 from config       import Config
 from extensions   import db, login_manager
@@ -7,17 +7,17 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # initialize extensions
+    # load in  stuff
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
-    # create tables
+    # create db
     with app.app_context():
         from models import User  # now safe
         db.create_all()
 
-    # register blueprints
+    # import bps
     from auth   import auth_bp
     from upload import upload_bp
     app.register_blueprint(auth_bp)
